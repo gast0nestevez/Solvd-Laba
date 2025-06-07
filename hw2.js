f7fefe649298f72391f741549912e15f3e1846c4
Gastón <93488817+gast0nestevez@users.noreply.github.com>
2025-06-07 13:02:14 -0300

    Add files via upload

---
+++ hw2.js
@@ -0,0 +1,141 @@
+/*
+# Homework 2
+
+Create a JavaScript library that provides advanced data transformation functions. The library should include the following features:
+
+**`addValues`**: Accepts two arguments of any type and performs the appropriate addition operation based on the types of
+the arguments. The function should return the result of the addition. If the addition is not possible, it should throw an error.
+
+**`stringifyValue`**: Accepts a single argument of any type and converts it to a string representation. For objects and
+arrays, use `JSON.stringify()` for serialization. For other types, use the appropriate built-in methods or operations to
+convert them to strings.
+
+**`invertBoolean`**: Accepts a single boolean argument and returns its inverted value. If the argument is not a boolean,
+it should throw an error.
+
+**`convertToNumber`**: Accepts a single argument of any type and attempts to convert it to a number. For strings, use
+`parseFloat()` or `parseInt()` for conversion. For other types, use appropriate operations or functions to perform the
+conversion. If the conversion is not possible, it should throw an error.
+
+**`coerceToType`**: Accepts two arguments: value and type. It attempts to convert the value to the specified type using type
+coercion. The function should return the coerced value if successful. If the coercion is not possible, it should throw an error.
+
+Implement additional functions of your choice that demonstrate advanced type conversion scenarios or cater to specific use
+cases related to primitive types. You are encouraged to explore complex scenarios and push the limits of type conversion.
+*/
+
+function sumArrays(a, b) {
+  if (a.length !== b.length) {
+    throw new Error('Arrays must be the same size')
+  }
+  return a.map((val, i) => val + b[i])
+}
+
+function addValues(a, b) {
+  let res = undefined
+
+  // Arithmetic addition 
+  if (typeof a === 'number') {
+    res = a + convertToNumber(b)
+  }
+  // String concatenation
+  else if (typeof a === 'string') {
+    res = a + stringifyValue(b)
+  }
+  // Array concatenation
+  else if (Array.isArray(a) && Array.isArray(b)) {
+    res = sumArrays(a, b)
+  }
+  // I've decided to omit boolean values, as I don't see any meaningful addition operation applicable to them
+
+  if (typeof res === 'undefined') {
+    throw new Error('Unsupported types')
+  }
+  return res
+}
+
+function stringifyValue(value) {
+  if (typeof value === 'object') {
+    return JSON.stringify(value)
+  }
+  return String(value)
+}
+
+function invertBoolean(value) {
+  if (typeof value !== 'boolean') {
+    throw new Error('Value must be a boolean')
+  }
+  return !value
+}
+
+function convertToNumber(value) {
+  const num = Number(value)
+  if (isNaN(num)) throw new Error('Invalid cast')
+  return num
+}
+
+function coerceToType(value, type) {
+  switch (type) {
+    case 'boolean':
+      return Boolean(value)
+
+    case 'string':
+      return stringifyValue(value)
+
+    case 'number':
+      return convertToNumber(value)
+
+    case 'array':
+      if (Array.isArray(value)) return value
+      return [value]
+
+    default:
+      throw new Error(`Unsupported type coercion to ${type}`)
+  }
+}
+
+console.log('-------- TESTS --------')
+
+console.log('addValues(2, 3) ->', addValues(2, 3))
+console.log('addValues(2, "8") ->', addValues(2, "8"))
+console.log('addValues(2, true) ->', addValues(2, true))
+console.log('addValues("Hello", " world!") ->', addValues('Hello', ' world!'))
+console.log('addValues("Value: ", 60) ->', addValues('Value: ', 60))
+console.log('addValues("Value: ", false) ->', addValues('Value: ', false))
+console.log('addValues([1, 2], [3, 4]) ->', addValues([1, 2], [3, 4]))
+
+console.log('-----------------------')
+
+console.log('stringifyValue(5) ->', stringifyValue(5))
+console.log('stringifyValue(true) ->', stringifyValue(true))
+console.log('stringifyValue("10") ->', stringifyValue('10'))
+console.log('stringifyValue({a: 1, b: 2, c: 3}) ->', stringifyValue({a: 1, b: 2, c: 3}))
+
+console.log('-----------------------')
+
+console.log('invertBoolean(false) ->', invertBoolean(false))
+console.log('invertBoolean(true) ->', invertBoolean(true))
+console.log('invertBoolean(true && false) ->', invertBoolean(true && false))
+
+console.log('-----------------------')
+
+console.log('convertToNumber(10) ->', convertToNumber(10))
+console.log('convertToNumber("10") ->', convertToNumber('10'))
+console.log('convertToNumber(false) ->', convertToNumber(false))
+
+console.log('-----------------------')
+
+console.log('coerceToType(1, "boolean") ->', coerceToType(1, 'boolean'))
+console.log('coerceToType(0, "boolean") ->', coerceToType(0, 'boolean'))
+console.log('coerceToType("1", "number") ->', coerceToType('1', 'number'))
+console.log('coerceToType(3.14, "string") ->', coerceToType(3.14, 'string'))
+console.log('coerceToType("6", "array") ->', coerceToType('6', 'array'))
+
+console.log('-----------------------')
+
+module.exports = {
+  addValues,
+  stringifyValue,
+  invertBoolean,
+  coerceToType
+}
